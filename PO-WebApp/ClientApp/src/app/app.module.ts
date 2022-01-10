0430670aec8638093272275320d01698bbd72d2f
Guilherme <guilherme.katsui@tvfsoftware.com>
2022-01-10 17:47:11 -0300

    PRIMEIRO COMIT

--- PO-WebApp/ClientApp/src/app/app.module.ts
+++ PO-WebApp/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FooterComponent } from './footer/footer.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ContatoComponent,
+    FooterComponent,
+    PainelComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +32,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'contato', component: ContatoComponent },
+      { path: 'painel', component: PainelComponent },
     ])
   ],
   providers: [],
