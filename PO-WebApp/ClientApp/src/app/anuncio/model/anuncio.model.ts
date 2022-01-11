3e983c82133685535a664c688d973e47e146d27e
Guilherme <guilherme.katsui@tvfsoftware.com>
2022-01-11 12:49:20 -0300

    ajustes

---
+++ PO-WebApp/ClientApp/src/app/anuncio/model/anuncio.model.ts
@@ -0,0 +1,9 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+export class AnuncioModel {
+  id: number;
+  codigo: string;
+  nome: string;
+  detalhe: string;
+  foto: string;
+}
--- PO-WebApp/ClientApp/src/app/home/home.component.ts
+++ PO-WebApp/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AnuncioModel } from '../anuncio/model/anuncio.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,14 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
 })
 export class HomeComponent
 {
+  anuncio: AnuncioModel[] = [
+    { id: 4, codigo: '000001', nome: 'Skate montado', detalhe: 'Semi-profissional-Shape de Maple Canadense', foto: 'skate_montado_profissional.jpeg' },
+    { id: 7, codigo: '000002', nome: 'tênis de skate', detalhe: 'Pro-model Serginho Santoro', foto: 'boot.jpg' },
+    { id: 9, codigo: '000003', nome: 'Shape', detalhe: 'Hape Elemnt 8.125', foto: 'shape.jpg' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg' }
+  ];
+
   constructor() {
   }
 }
+
