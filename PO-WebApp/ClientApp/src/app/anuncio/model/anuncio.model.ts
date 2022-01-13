c752fbbd9ea45ef1f0f70f21e02b8d50315de17f
Guilherme <guilherme.katsui@tvfsoftware.com>
2022-01-13 12:18:28 -0300

    ajuste no grid

--- PO-WebApp/ClientApp/src/app/anuncio/model/anuncio.model.ts
+++ PO-WebApp/ClientApp/src/app/anuncio/model/anuncio.model.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 export class AnuncioModel {
   id: number;
   codigo: string;
   nome: string;
   detalhe: string;
   foto: string;
+  logo: string;
+  empresa: string;
 }
--- PO-WebApp/ClientApp/src/app/home/home.component.ts
+++ PO-WebApp/ClientApp/src/app/home/home.component.ts
@@ -9,10 +9,16 @@ import { AnuncioModel } from '../anuncio/model/anuncio.model';
 export class HomeComponent
 {
   anuncio: AnuncioModel[] = [
-    { id: 4, codigo: '000001', nome: 'Skate montado', detalhe: 'Semi-profissional-Shape de Maple Canadense', foto: 'skate_montado_profissional.jpeg' },
-    { id: 7, codigo: '000002', nome: 'tênis de skate', detalhe: 'Pro-model Serginho Santoro', foto: 'boot.jpg' },
-    { id: 9, codigo: '000003', nome: 'Shape', detalhe: 'Hape Elemnt 8.125', foto: 'shape.jpg' },
-    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg' }
+    { id: 4, codigo: '000001', nome: 'Conjunto', detalhe: 'Mundo Feliz', foto: 'skate_montado_profissional.jpeg', logo:'independent.png',empresa: 'independent' },
+    { id: 7, codigo: '000002', nome: 'tênis de skate', detalhe: 'Pro-model Serginho Santoro', foto: 'boot.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 9, codigo: '000003', nome: 'Shape', detalhe: 'Hape Elemnt 8.125', foto: 'shape.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent' },
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent'},
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent'},
+    { id: 5, codigo: '00000', nome: 'camiseta', detalhe: 'Dgk street survival', foto: 'dgk.jpg', logo: 'independent.png', empresa: 'independent' }
   ];
 
   constructor() {
